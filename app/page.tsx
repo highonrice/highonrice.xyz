06d7f8ae5c8380b5bf775f4710ea639256cac4ba
Steve <ansndmin1@gmail.com>
2025-01-09 23:32:26 +0900

    feat: make product manager text semibold

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold mb-4">highonrice.base.eth</h1>
         <p className="text-lg text-gray-700 font-semibold mb-4">a professional baby stepper</p>
         <p className="text-lg text-gray-700">currently working at <a href="https://kroma.network" target="_blank" rel="noopener noreferrer" className="underline">kroma</a>.</p>
-        <p className="text-lg text-gray-700 mb-4">as a developer-turned-product manager with a user-centric mindset, I excel in planning, executing and managing complex services and UX while understanding both business and technology.</p>
+        <p className="text-lg text-gray-700 mb-4">as a <span className='text-lg text-gray-700 font-semibold'>developer-turned-product manager</span> with a user-centric mindset, I excel in planning, executing and managing complex services and UX while understanding both business and technology.</p>
         <p className="text-lg text-gray-700">you can find my professional journey on <a href="https://linkedin.com/in/seoho-yoon" target="_blank" rel="noopener noreferrer" className="text-gray-700 underline">LinkedIn</a>,</p>
         <p className="text-lg text-gray-700">shitposting with degen mode on <a href="https://x.com/0xdonsterling" target="_blank" rel="noopener noreferrer" className="text-gray-700 underline">X(formerly Twitter)</a>,</p>
         <p className="text-lg text-gray-700">building toy projects on <a href="https://warpcast.com/0xdonsterling" target="_blank" rel="noopener noreferrer" className="text-gray-700 underline">Warpcast</a>,</p>
